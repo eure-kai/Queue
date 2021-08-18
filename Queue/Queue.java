7f425ef71b17bc1a2d44cf19479792b7a01fe86f
Kai Kim <86928667+eure-kai@users.noreply.github.com>
2021-08-18 16:43:12 -0400

    Update and rename Queue.java to Queue/Queue.java

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,42 +1,41 @@
 public class Queue {
-  private final int MAX;	// maximum capacity of the queue
-	private int[] queue; // array to store queue elements
-	private int front; // the start of the queue
-	private int nextBack;	// where to insert the next item in the back
-	private int size;		// current size of the queue
-
-	// Constructor to initialize queue
+	private final int MAX;	
+	private int[] queue; 
+	private int front; 
+	private int nextBack;	
+	private int size;		
+	
+	
 	public Queue(){
-    MAX = 1000;
-    queue = new int[MAX];
+    		MAX = 1000;
+    		queue = new int[MAX];
 		front = 0;
 		nextBack = 0;
 		size = 0;
 	}
 
-  // Returns whether the queue is empty or not
+
 	public Boolean isEmpty(){
 		return (size == 0);
 	}
 
-	// Returns whether the queue is full or not
 	public Boolean isFull(){
 		return (size == MAX);
 	}
 
-	// Remove front element from the queue
+	
 	public int dequeue() {
 	  
 	  if (isEmpty()) throw new UnderflowException();
-	  
+
 	  int temp = queue[front];
 	  front = (front + 1) % MAX;
 	  size--;
-	  
+
 	  return temp;
 	}
 
-	// Add an item to the back of the queue
+	
 	public void enqueue(int item) {
 	  if (isFull()) throw new OverflowException();
 	  
@@ -45,13 +44,13 @@ public class Queue {
 	  size++;
 	}
 
-	// Returns the front element in the queue
+	
 	public int front() {
 	  if (isEmpty()) throw new UnderflowException();
-    return queue[front];
+    	  return queue[front];
 	}
 
-	// Returns the size of the queue
+	
 	public int size() {
 		return size;
 	}
