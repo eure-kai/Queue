6b641903ef2db968499644f425a133c1bf4851d1
Kai Kim <86928667+eure-kai@users.noreply.github.com>
2021-08-18 16:48:06 -0400

    Update Queue.java

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,84 +1,83 @@
 public class Queue {
-	private final int MAX;	
-	private int[] queue; 
-	private int front; 
-	private int nextBack;	
-	private int size;		
+  private final int MAX;	
+  private int[] queue; 
+  private int front; 
+  private int nextBack;	
+  private int size;		
 	
 	
-	public Queue(){
-    		MAX = 1000;
-    		queue = new int[MAX];
-		front = 0;
-		nextBack = 0;
-		size = 0;
-	}
+  public Queue(){
+    MAX = 1000;
+    queue = new int[MAX];
+    front = 0;
+    nextBack = 0;
+    size = 0;
+  }
 
 
-	public Boolean isEmpty(){
-		return (size == 0);
-	}
+  public Boolean isEmpty(){
+    return (size == 0);
+  }
 
-	public Boolean isFull(){
-		return (size == MAX);
-	}
+  public Boolean isFull(){
+    return (size == MAX);
+  }
 
-	
-	public int dequeue() {
-	  
-	  if (isEmpty()) throw new UnderflowException();
 
-	  int temp = queue[front];
-	  front = (front + 1) % MAX;
-	  size--;
+  public int dequeue() {
 
-	  return temp;
-	}
+    if (isEmpty()) throw new UnderflowException();
 
-	
-	public void enqueue(int item) {
-	  if (isFull()) throw new OverflowException();
-	  
-	  queue[nextBack] = item;
-	  nextBack = (nextBack + 1) % MAX;
-	  size++;
-	}
+    int temp = queue[front];
+    front = (front + 1) % MAX;
+    size--;
 
-	
-	public int front() {
-	  if (isEmpty()) throw new UnderflowException();
-    	  return queue[front];
-	}
+    return temp;
+  }
 
-	
-	public int size() {
-		return size;
-	}
+
+  public void enqueue(int item) {
+    if (isFull()) throw new OverflowException();
+
+    queue[nextBack] = item;
+    nextBack = (nextBack + 1) % MAX;
+    size++;
+  }
+
+
+  public int front() {
+    if (isEmpty()) throw new UnderflowException();
+    return queue[front];
+  }
+
+
+  public int size() {
+    return size;
+  }
 
   @Override
   public String toString() {
     return ("[" + printQueue() + "]");
   }
-  
+
   public String printQueue() {
     if (size == 0) {
       return "";
-      
+
     } else if (size == 1) {
       return String.valueOf(queue[front]);
-      
+
     } else {
-      
       StringBuilder wordBuilder = new StringBuilder();
-    
+
       for (int i = 0; i < size; i++) {
-        int index = (front + i) % MAX;
-        wordBuilder.append(queue[index]);
-        if (i != (size - 1)) wordBuilder.append(", ");
+	int index = (front + i) % MAX;
+	wordBuilder.append(queue[index]);
+	if (i != (size - 1)) wordBuilder.append(", ");
       }
-    
-      return wordBuilder.toString();
+
+    return wordBuilder.toString();
     }
   }
-  
+	
 }
